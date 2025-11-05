d3bfe3495503d6d7bbaaf4789e84e7c3865d4e73
Luis Contreras <lcontrerasperaza99@gmail.com>
2025-11-05 01:31:58 -0700

    aquila exile from iter folder

--- frontend/src/api/auth.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import API_BASE_URL from '../config/apiConfig.js';
-
-
-export const loginRequest = async (user) => {
-    return await axios.post(`${API_BASE_URL}/api/auth/login`, user, { withCredentials: true });
-}
-
